1386a27042c9a8e4f95905e7e4ae7760fcb9b39a
dufournaudy <Yves.Dufournaud@moodys.com>
2020-07-20 17:35:23 +0200

    [where]

--- test.rb
+++ test.rb
@@ -2,5 +2,5 @@ require_relative 'sql'
 
 load('sql.rb')
 m = Parser.parse(' select  /*+ first_rows(1) */ a.A,b,d as cd from d.d where 1=1')
-    .ast.flatten.compact
-
+m.ast.flatten.compact
+m.as_sql
